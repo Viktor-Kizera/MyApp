de122ce3db24aedb67a1ae5ada39773370fe2d22
Viktor-Kizera <victorkizera2003@gmail.com>
2024-03-29 17:46:37 +0200

    First version of program

---
+++ MyName/MyName/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyName
+//
+//  Created by Viktor Kizera on 29.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    //
+}
+
