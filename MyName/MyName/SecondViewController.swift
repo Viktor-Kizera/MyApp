94943e11cb4b2c28275fe65198828f6f0e770157
Viktor-Kizera <victorkizera2003@gmail.com>
2024-03-30 17:03:34 +0200

    realize myName app on UiKit

--- MyName/MyName/SecondViewController.swift
+++ MyName/MyName/SecondViewController.swift
@@ -9,13 +9,24 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+  @IBOutlet weak var myLabel: UILabel!
+    
+    
+  @IBAction func showAlert() {
+      let alertController = UIAlertController(title: "Welcome", message: "This is myName app", preferredStyle: .alert)
+      let actionOK = UIAlertAction(title: "OK", style: .default, handler: nil)
+      let actionCancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+      alertController.addAction(actionOK)
+      alertController.addAction(actionCancel)
+      self.present(alertController, animated: true, completion: nil)
     }
     
-
+    
+    @IBAction func changeLabelText(_ sender: UIButton) {
+        if let buttonText = sender.titleLabel!.text {
+            myLabel.text = "\(buttonText) button was pressed"
+        }
+    }
     /*
     // MARK: - Navigation
 
